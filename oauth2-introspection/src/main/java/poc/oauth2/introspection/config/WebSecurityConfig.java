c8054c617ae6c30b8afe9cf1855dbb1ce6aeaa15
mozes.hajdu <mozes.hajdu@areus.hu>
2023-08-10 13:25:52 +0200

    replace deprecated opaque token

--- oauth2-introspection/src/main/java/poc/oauth2/introspection/config/WebSecurityConfig.java
+++ oauth2-introspection/src/main/java/poc/oauth2/introspection/config/WebSecurityConfig.java
@@ -1,11 +1,14 @@
 package poc.oauth2.introspection.config;
 
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
@@ -17,7 +20,10 @@ import java.util.List;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class WebSecurityConfig {
+    OAuth2ResourceServerProperties oAuth2ResourceServerProperties;
 
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
@@ -25,7 +31,10 @@ public class WebSecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(registry -> registry.anyRequest().authenticated())
                 .sessionManagement(sessionManagementConfigurer -> sessionManagementConfigurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .oauth2ResourceServer(OAuth2ResourceServerConfigurer::opaqueToken);
+                .oauth2ResourceServer(resourceServer -> resourceServer.
+                        opaqueToken(opaqueToken -> opaqueToken
+                                .introspectionUri(oAuth2ResourceServerProperties.getOpaquetoken().getIntrospectionUri())
+                                .introspectionClientCredentials(oAuth2ResourceServerProperties.getOpaquetoken().getClientId(), oAuth2ResourceServerProperties.getOpaquetoken().getClientSecret())));
         return http.build();
     }
 
